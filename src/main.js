85d5e7b27664d10fc098ef2056f4530aa1f5113c
Deniz <candenizozder08@gmail.com>
2025-06-18 19:37:23 +0300

    day 06 v1

--- src/main.js
+++ src/main.js
@@ -7,18 +7,31 @@
       burger.classList.toggle('active');
     });
   });
+
+  // Edu BTN
+
+  document.addEventListener('DOMContentLoaded', function () {
+    const eduBTN = document.getElementById('edu-button');
+    const details = document.getElementById('detail-btn');
+
+    eduBTN.addEventListener('click', () => {
+      details.classList.toggle('shown');
+      eduBTN.classList.toggle('activebtn');
+    });
+  });
   
   document.addEventListener("DOMContentLoaded", () => {
     const sections = document.querySelectorAll("section[id]");
     const navLinks = document.querySelectorAll(".nav-link");
+    const interest = document.getElementById('Interest');
 
     function getOffsetByDevice() {
       const width = window.innerWidth;
 
       if (width < 576) return 455;           // Mobile
-      if (width >= 576 && width < 992) return 80;   // Tablet
-      return 100;                           // Desktop
-    }
+      if (width >= 576 && width < 992) return 150;   // Tablet
+      return 150;                           // Desktop
+      }
 
     function onScroll() {
       const scrollY = window.pageYOffset;
@@ -58,5 +71,19 @@
     body.classList.toggle('dark-theme');
     const isLight = body.classList.contains('light-theme');
     localStorage.setItem('theme', isLight ? 'light' : 'dark');
+  });
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+  const eduListItems = document.querySelectorAll('.edu-lesson');
+
+  eduListItems.forEach(item => {
+    const iconRotate = item.querySelector('.icon-lesson');
+    const eduLessons = item.querySelector('.item-list');
+
+    item.addEventListener('click', () => {
+      iconRotate.classList.toggle('rotated');
+      eduLessons.classList.toggle('visible');
+    });
   });
 });
